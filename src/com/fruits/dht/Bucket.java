9d648a1b7cfa728fd0bdfbabf77885ac43348892
jastang <TomHornson@hotmail.com>
2019-10-28 08:29:11 +0800

    + fix bug of Bucket & RoutingTable

--- src/com/fruits/dht/Bucket.java
+++ src/com/fruits/dht/Bucket.java
@@ -12,9 +12,92 @@ public class Bucket {
 
     private Bucket next;
 
-    public Bucket(int minIndex, int maxIndex) {
+    private RoutingTable routingTable;
+
+    public Bucket(int minIndex, int maxIndex, RoutingTable routingTable) {
         this.minIndex = minIndex;
         this.maxIndex = maxIndex;
+        this.routingTable = routingTable;
+    }
+
+    /*
+        Logic of putNodeInBucket:
+
+     */
+
+    public void putNode(Node node) {
+        // TODO: should I lock the nodes?
+
+        if(nodes.contains(node)) {
+            /**
+             * Removes a single instance of the specified element from this queue,
+             * if it is present.  More formally, removes an element {@code e} such
+             * that {@code o.equals(e)}, if this queue contains one or more such
+             * elements.
+             * Returns {@code true} if this queue contained the specified element
+             * (or equivalently, if this queue changed as a result of the call).
+             *
+             * <p>Removal of interior elements in circular array based queues
+             * is an intrinsically slow and disruptive operation, so should
+             * be undertaken only in exceptional circumstances, ideally
+             * only when the queue is known not to be accessible by other
+             * threads.
+             *
+             * @param o element to be removed from this queue, if present
+             * @return {@code true} if this queue changed as a result of the call
+             */
+            // TODO: risky?
+            nodes.remove(node);
+            try{
+                nodes.put(node);
+
+                routingTable.putNode(node);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        }else{
+            if(nodes.remainingCapacity() > 0) {
+                try{
+                    //TODO: should I put the node in another sorted indexed sequential container?
+                    nodes.put(node);
+
+                    routingTable.putNode(node);
+                }catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                // bucket is full,
+                // if myself is in this bucket, split this bucket.
+                int selfNodeIndex = Utils.getLog2(Utils.hexStringToBytes(DHTClient.selfNodeId));
+
+                if(selfNodeIndex >= minIndex && selfNodeIndex < maxIndex) {
+                    int range = maxIndex - minIndex;
+                    if(range > 1) { // at least, this bucket could be spliced into two bucket
+                        int half = range/2;
+
+                        int newIndex = (minIndex + half);
+
+                        this.maxIndex = newIndex;
+
+                        Bucket newBucket = new Bucket(newIndex, maxIndex, routingTable);
+
+                        // relocate the nodes in current bucket
+                        for(Node o : nodes) {
+                            if(o.getBucketIndex() >= newIndex) {
+                                // TODO: put the node in a sorted indexed sequential container,
+                                newBucket.putNode(o);
+                                nodes.remove(o);
+                            }
+                        }
+
+                        newBucket.next = this.next;
+                        this.next = newBucket;
+
+                        routingTable.putNodeInBucket(node);
+                    }
+                }
+            }
+        }
     }
 
     public Bucket getNext() {
@@ -39,69 +122,5 @@ public class Bucket {
 
     public void setMaxIndex(int maxIndex) {
         this.maxIndex = maxIndex;
-    }
-
-    public void addNode(Node node) {
-        if(nodes.contains(node)) {
-            /**
-             * Removes a single instance of the specified element from this queue,
-             * if it is present.  More formally, removes an element {@code e} such
-             * that {@code o.equals(e)}, if this queue contains one or more such
-             * elements.
-             * Returns {@code true} if this queue contained the specified element
-             * (or equivalently, if this queue changed as a result of the call).
-             *
-             * <p>Removal of interior elements in circular array based queues
-             * is an intrinsically slow and disruptive operation, so should
-             * be undertaken only in exceptional circumstances, ideally
-             * only when the queue is known not to be accessible by other
-             * threads.
-             *
-             * @param o element to be removed from this queue, if present
-             * @return {@code true} if this queue changed as a result of the call
-             */
-            // TODO: risky?
-            nodes.remove(node);
-            try{
-                nodes.put(node);}
-            catch(InterruptedException e) {
-                e.printStackTrace();
-            }
-        }else{
-            if(nodes.remainingCapacity() > 0) {
-                try{
-                    //TODO: put the node in a sorted indexed sequential container,
-                    nodes.put(node);
-                }catch(InterruptedException e) {
-                    e.printStackTrace();;
-                }
-            }else{
-                int selfNodeIndex = Utils.getLog2(Utils.hexStringToBytes(DHTClient.selfNodeId));
-                if(selfNodeIndex >= minIndex && selfNodeIndex < maxIndex) {
-                    int range = maxIndex - minIndex;
-                    if(range > 1) {
-                        int half = range/2;
-
-                        int newIndex = (minIndex + half);
-
-                        this.maxIndex = newIndex;
-
-                        Bucket newBucket = new Bucket(newIndex, maxIndex);
-
-                        // relocate the nodes in current bucket
-                        for(Node o : nodes) {
-                            if(o.getBucketIndex() >= newIndex) {
-                                // TODO: put the node in a sorted indexed sequential container,
-                                newBucket.addNode(o);
-                                nodes.remove(o);
-                            }
-                        }
-
-                        newBucket.next = this.next;
-                        this.next = newBucket;
-                    }
-                }
-            }
-        }
     }
 }
--- src/com/fruits/dht/RoutingTable.java
+++ src/com/fruits/dht/RoutingTable.java
@@ -6,22 +6,31 @@ public class RoutingTable {
     public static final int BUCKET_MIN_INDEX = 0;
     public static final int BUCKET_MAX_INDEX = 160; // exclusive
 
-    private Bucket head = new Bucket(BUCKET_MIN_INDEX, BUCKET_MAX_INDEX);
+    // initial bucket.
+    private Bucket head = new Bucket(BUCKET_MIN_INDEX, BUCKET_MAX_INDEX, this);
     public static List<Node> nodes = new ArrayList<Node>();
 
-
+    // TODO: PingThread may call this method
     public List<Node> getNodes() {
         return this.nodes;
     }
 
+    // TODO: PingThread may call this method.
     public void removeNode(Node node) {
         this.nodes.remove(node);
     }
 
-    public void addNode(Node node) {
+    // TODO: only Bucket could call this method.
+    public void putNode(Node node) {
+        this.nodes.add(node);
+    }
+
+    // TODO:
+    public boolean putNodeInBucket(Node node) {
+        // TODO: node must have a node id.
         String nodeId = node.getId();
         if(nodeId == null)
-            return;
+            return false;
 
         // nodeId is a 160 bits string
         // bucketIndex range: 0 - 160(exclusive)
@@ -44,8 +53,11 @@ public class RoutingTable {
         }
 
         if(bucket != null) {
-            bucket.addNode(node);
+            bucket.putNode(node);
+            return true;
         }
+
+        return false;
     }
 
     // TODO:
@@ -55,44 +67,45 @@ public class RoutingTable {
     // this api works for find_node request.
     // if target node is found, return it otherwise return 8 closest nodes
     // TODO: important function, need to be very careful about the details.
-    public List<Node> getClosest8Nodes(String nodeId) {
-        List<Node> expectedNodes = new ArrayList<Node>();
+    public List<Node> getClosest8Nodes(String nodeId/* nodeId could be a infohash value*/) {
+        List<Node> foundNodes = new ArrayList<Node>();
 
-        for(Node node : nodes) {
-            if(node.getId().equals(nodeId)) {
-                expectedNodes.add(node);
-                return expectedNodes;
-            }
-        }
-
-        // sort the nodes by distance from 'node' then return the closest 8 nodes
-
-        Node[] nodesArray = (Node[])nodes.toArray();
-
-        Arrays.sort(nodesArray, new Comparator() {
-            @Override
-            public int compare(Object o1, Object o2) {
-                // TODO: special handling, e.g. null assert,
-                Bitmap distance1 = new Bitmap(Utils.getDistance(((Node)o1).getId(), nodeId));
-                Bitmap distance2 = new Bitmap(Utils.getDistance(((Node)o2).getId(), nodeId));
-                int length = distance1.length();
-
-                for(int i = 0; i < length; i++) {
-                    if(distance1.get(i) && !distance2.get(i)) {
-                        return 1;
-                    }else if(!distance1.get(i) && distance2.get(i)) {
-                        return -1;
-                    }
+        if(nodes.size() > 0) {
+            for (Node node : nodes) {
+                if (node.getId().equals(nodeId)) {
+                    foundNodes.add(node);
+                    // only one node will be returned, we have found the exact node.
+                    return foundNodes;
                 }
-
-                return 0;
             }
-        });
 
-        for(int i = 0; i < nodesArray.length && i < Bucket.BUCKET_CAPACITY_MAX; i++) {
-            expectedNodes.add(nodesArray[i]);
+            // sort the nodes by distance from 'node' then return the closest 8 nodes
+            Node[] nodesArray = (Node[]) nodes.toArray(new Node[0]);
+
+            Arrays.sort(nodesArray, new Comparator() {
+                @Override
+                public int compare(Object o1, Object o2) {
+                    // TODO: special handling, e.g. null assert,
+                    Bitmap distance1 = new Bitmap(Utils.getDistance(((Node) o1).getId(), nodeId));
+                    Bitmap distance2 = new Bitmap(Utils.getDistance(((Node) o2).getId(), nodeId));
+                    int size = distance1.size();
+
+                    for (int i = 0; i < size; i++) {
+                        if (distance1.get(i) && !distance2.get(i)) {
+                            return 1;
+                        } else if (!distance1.get(i) && distance2.get(i)) {
+                            return -1;
+                        }
+                    }
+
+                    return 0;
+                }
+            });
+
+            for (int i = 0; i < nodesArray.length && i < Bucket.BUCKET_CAPACITY_MAX; i++) {
+                foundNodes.add(nodesArray[i]);
+            }
         }
-
-        return expectedNodes;
+        return foundNodes;
     }
 }
